1152442a4751e7d8e3746ac5e69085b21688243d
kasaki-nagato <kasaki-nagato@dmm.com>
2019-01-09 17:33:47 +0900

    add chap 2 - 7 empty files

---
+++ 2/1/2-1-2-1_SimpleStorage.sol
@@ -0,0 +1,5 @@
+pragma solidity >=0.4.0 <0.6.0;
+
+contract SimpleStorage { // コントラクトの宣言
+    // ...
+}
---
+++ 2/1/2-1-2-2_SimpleStorage.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.4.0 <0.6.0;
+
+contract SimpleStorage {
+    uint storedData; // 状態変数
+    // ...
+}
