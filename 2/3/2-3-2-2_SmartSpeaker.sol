903a874a0f8fcadd84cd140870cbc9d67790ed22
AkiraMAEGAWA <maeaki112@gmail.com>
2019-01-10 12:32:55 +0900

    add 2-3 sample codes

---
+++ 2/3/2-3-2-2_SmartSpeaker.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.24;
+
+contract SmartSpeaker {
+
+    // publicなstate variableは自動的にgetterが生成される
+    string public response;
+
+    constructor () public {
+        response = "";
+    }
+
+    function listen (uint _number) public returns(string memory) {
+        if (_number <= 100) {
+            response = "small!";
+        } else if (_number <= 1000) {
+            response = "normal!!";
+        } else if (_number <= 10000) {
+            response = "large!!!";
+        } else if (_number <= 100000) {
+            response = "WOWOWO!!!!";
+        } else {
+            response = "unbelievable!!!!!";
+        }
+
+        return response;
+    }
+}
