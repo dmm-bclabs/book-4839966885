64330f5f07a48e833774cf1ba58d5795bb64a0ee
ecouffes <tanakakatsunori2@gmail.com>
2019-01-14 14:20:02 +0900

    remove lines

--- 2/2/2-2-4-3_Election.sol
+++ 2/2/2-2-4-3_Election.sol
@@ -1,11 +1,9 @@
 pragma solidity >=0.4.24 <0.6.0;
 
 contract SimpleElection {
-
     mapping (address => string) public OwnerToCandidate;
 
     function elect(string memory _candidateName) public {
         OwnerToCandidate[msg.sender] = _candidateName;
     }
-
 }
