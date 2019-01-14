94bca82fd287fc478bd8497f2f74d4d5849a7b15
ecouffes <tanakakatsunori2@gmail.com>
2019-01-14 14:33:24 +0900

    remove files

--- 2/2/2-2-4-3_Election.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.4.24 <0.6.0;
-
-contract SimpleElection {
-    mapping (address => string) public OwnerToCandidate;
-
-    function elect(string memory _candidateName) public {
-        OwnerToCandidate[msg.sender] = _candidateName;
-    }
-}
